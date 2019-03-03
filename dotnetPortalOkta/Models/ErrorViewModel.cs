40b1088511bd7fef24ea9a37eaccdf3df239e2c2
John (Jef) Fisher <Fishe545@gmail.com>
2019-03-03 08:04:02 -0800

    first commit

---
+++ dotnetPortalOkta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetPortalOkta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
